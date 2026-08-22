af189fee825c725194e393d26b64a4ff39a10a44
Suchit405 <suchitkumar918@gmail.com>
2026-08-23 02:29:08 +0530

    Add: subarray-product-less-than-k - submission-8

---
+++ Data Structures & Algorithms/subarray-product-less-than-k/submission-8.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k <= 1:
+            return 0
+        l, r = 0, 0
+        product = 1
+        res = 0
+        for r in range(len(nums)):
+            product *= nums[r]
+            while product >= k:
+                product /= nums[l]
+                l += 1
+            res += r - l + 1
+        return res
