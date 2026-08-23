73955edf10f712867082052a88995e817f3f0a68
Suchit405 <suchitkumar918@gmail.com>
2026-08-23 21:16:01 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        hash_map = {")" : "(", "}" : "{", "]" : "["}
+        a = []
+        for i in s:
+            if i in hash_map and len(a) > 0:
+                if a[-1] == hash_map[i]:
+                    a.pop()
+                else:
+                    return False
+                continue
+            a.append(i)
+        if len(a) == 0:
+            return True
+        return False
