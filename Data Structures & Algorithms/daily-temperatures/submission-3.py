318a6057d5ddb343e68f9ace1e1e6fb2b62a9d1e
Suchit405 <suchitkumar918@gmail.com>
2026-08-24 23:02:49 +0530

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        monotonic_stack = []
+        for i in range(len(temperatures) - 1, -1, -1):
+            while monotonic_stack and temperatures[i] >= temperatures[monotonic_stack[-1]]:
+                monotonic_stack.pop()
+            if monotonic_stack:
+                res[i] = (monotonic_stack[-1] - i)
+            monotonic_stack.append(i)
+            # res[i] = (monotonic_stack[-2] if len(monotonic_stack) >= 2 else 0)
+        return res
